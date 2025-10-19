66fbf5213a922ee25592d58089b541c6b636eced
JathinB <jathin.b@mckinneysteamacademy.org>
2025-10-19 13:06:57 -0500

    Auton 1Piece

--- src/main/java/frc/robot/subsystems/manipulator/Manipulator.java
+++ src/main/java/frc/robot/subsystems/manipulator/Manipulator.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems.manipulator;
 
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -80,6 +82,9 @@ public class Manipulator extends SubsystemBase {
     kHardware.setVoltage(kRollerOuttakeVoltage);
   }
 
+  public Command outtakeCommand() {
+    return Commands.sequence(Commands.runOnce(() -> {mIntakeState = IntakeState.OUTTAKING;}), Commands.runOnce(() -> {kHardware.setVoltage(kRollerOuttakeVoltage);}));
+  }
   
 
   public boolean getCoralDetected() {
