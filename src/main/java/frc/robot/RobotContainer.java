71057b6cb724eff15e1b43fbf13ed7f2f3a9d63a
JathinB <jathin.b@mckinneysteamacademy.org>
2025-10-24 23:48:47 -0500

    hehehe speed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -73,6 +73,8 @@ public class RobotContainer {
     // Define other utility classes
     
     private LoggedDashboardChooser<String> autoChooser;
+    private LoggedDashboardChooser<Double> accelerationChooser;
+
     
     private final CommandXboxController driverController = new CommandXboxController(0);
     private final CommandXboxController operatorController = new CommandXboxController(1);
@@ -99,6 +101,7 @@ public class RobotContainer {
     }
 
     public RobotContainer() {
+    
         // If using AdvantageKit, perform mode-specific instantiation of subsystems.
         switch (Constants.kCurrentMode) {
             case REAL:
@@ -183,7 +186,12 @@ public class RobotContainer {
         // Instantiate subsystems that don't care about mode, or are non-AdvantageKit enabled.
         // ex: LEDs = new LEDSubsystem();
         createAutos();
-
+        accelerationChooser = new LoggedDashboardChooser<Double>("Teleop/Acceleration Chooser");
+        accelerationChooser.addOption("A.B. (0.1)", 0.1);
+        accelerationChooser.addOption("SNAIL", 0.5);
+        accelerationChooser.addOption("SLOW", 0.7);
+        accelerationChooser.addOption("NORMAL", 1.0);
+        
         robotDrive.setDefaultCommand(Commands.run(() -> robotDrive.setDriveState(DriveState.TELEOP), robotDrive));
         //m_Elevator.setDefaultCommand(Commands.run(() -> m_Elevator.setGoal(ElevatorGoal.kStow), m_Elevator));
         //m_Intake.setDefaultCommand(Commands.run(()-> m_Intake.setPivotGoal(IntakePivotGoal.kStow), m_Intake));
@@ -193,10 +201,12 @@ public class RobotContainer {
         SlewRateLimiter slewFilterX = new SlewRateLimiter(0.7);
         SlewRateLimiter slewFilterY = new SlewRateLimiter(0.7);
 
+        double speedScalar = accelerationChooser.get();
+
         robotDrive.acceptJoystickInputs(
-            () -> - Math.copySign(driverController.getLeftY() * driverController.getLeftY(), driverController.getLeftY()),
-            () -> - Math.copySign(driverController.getLeftX() * driverController.getLeftX(), driverController.getLeftX()),
-            () -> driverController.getRightX() * (Constants.kCurrentMode == Mode.SIM ? -1.0 : 1.0),
+            () -> - Math.copySign(driverController.getLeftY() * driverController.getLeftY(), driverController.getLeftY()) * speedScalar,
+            () -> - Math.copySign(driverController.getLeftX() * driverController.getLeftX(), driverController.getLeftX()) * speedScalar,
+            () -> driverController.getRightX() * (Constants.kCurrentMode == Mode.SIM ? -1.0 : 1.0) * speedScalar,
             () -> driverController.getHID().getPOV());
 
 
