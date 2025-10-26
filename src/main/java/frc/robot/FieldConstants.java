efa28f38dac5bdc0c64e8b3756e6b57c569e87ab
JathinB <jathin.b@mckinneysteamacademy.org>
2025-10-26 08:38:46 -0500

    Update FieldConstants.java

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -10,24 +10,24 @@ public class FieldConstants {
     public static final Pose2d AR = new Pose2d(3.162, 3.872, Rotation2d.fromDegrees(0.0));
     public static final Pose2d AM = new Pose2d(average(AL.getX(), AR.getX()), average(AL.getY(), AR.getY()), Rotation2d.fromDegrees(0.0));
     
-    public static final Pose2d BL = new Pose2d(3.69, 2.97, Rotation2d.fromDegrees(60.0));
-    public static final Pose2d BR = new Pose2d(4.02, 2.79, Rotation2d.fromDegrees(60.0));
+    public static final Pose2d BL = new Pose2d(3.67, 2.976, Rotation2d.fromDegrees(60.0));
+    public static final Pose2d BR = new Pose2d(3.9617, 2.805, Rotation2d.fromDegrees(60.0));
     public static final Pose2d BM = new Pose2d(average(BL.getX(), BR.getX()), average(BL.getY(), BR.getY()), Rotation2d.fromDegrees(60.0));
     
-    public static final Pose2d CL = new Pose2d(4.99, 2.81, Rotation2d.fromDegrees(120.0));
-    public static final Pose2d CR = new Pose2d(5.3, 2.96, Rotation2d.fromDegrees(120.0));
+    public static final Pose2d CL = new Pose2d(4.996, 2.790, Rotation2d.fromDegrees(120.0));
+    public static final Pose2d CR = new Pose2d(5.28, 2.963, Rotation2d.fromDegrees(120.0));
     public static final Pose2d CM = new Pose2d(average(CL.getX(), CR.getX()), average(CL.getY(), CR.getY()), Rotation2d.fromDegrees(120.0));
     
-    public static final Pose2d DL = new Pose2d(5.81, 3.86, Rotation2d.fromDegrees(180.3));
-    public static final Pose2d DR = new Pose2d(5.81, 4.18, Rotation2d.fromDegrees(180.3));
+    public static final Pose2d DL = new Pose2d(5.801, 3.822, Rotation2d.fromDegrees(180.3));
+    public static final Pose2d DR = new Pose2d(5.801, 4.222, Rotation2d.fromDegrees(180.3));
     public static final Pose2d DM = new Pose2d(average(DL.getX(), DR.getX()), average(DL.getY(), DR.getY()), Rotation2d.fromDegrees(180.3));
     
-    public static final Pose2d EL = new Pose2d(5.27, 5.11, Rotation2d.fromDegrees(-120.0));
-    public static final Pose2d ER = new Pose2d(4.93, 5.21, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d EL = new Pose2d(5.309, 5.058, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d ER = new Pose2d(5.019, 5.240, Rotation2d.fromDegrees(-120.0));
     public static final Pose2d EM = new Pose2d(average(EL.getX(), ER.getX()), average(EL.getY(), ER.getY()), Rotation2d.fromDegrees(-120.0));
     
-    public static final Pose2d FL = new Pose2d(4.012, 5.250, Rotation2d.fromDegrees(-60.0)); //y
-    public static final Pose2d FR = new Pose2d(3.65, 5.02, Rotation2d.fromDegrees(-60.0)); //y
+    public static final Pose2d FL = new Pose2d(3.985, 5.251, Rotation2d.fromDegrees(-60.0));
+    public static final Pose2d FR = new Pose2d(3.696, 5.080, Rotation2d.fromDegrees(-60.0));
     public static final Pose2d FM = new Pose2d(average(FL.getX(), FR.getX()), average(FL.getY(), FR.getY()), Rotation2d.fromDegrees(-60.0));
 
     public static final Pose2d AD = new Pose2d(3.12, 4.386, Rotation2d.fromDegrees(0.0));          // reefABDescore
@@ -37,15 +37,19 @@ public class FieldConstants {
     public static final Pose2d ED = new Pose2d(5.456, 5.034, Rotation2d.fromDegrees(-120.0));       // reefIJDescore
     public static final Pose2d FD = new Pose2d(4.107, 5.391, Rotation2d.fromDegrees(-60.0));        // reefKLDescore
 
-    public static final Pose2d B_IL = new Pose2d(1.07, 7.06, Rotation2d.fromDegrees(-55.0)); // SourceLeft
-    public static final Pose2d B_IR = new Pose2d(1.0103, 0.926, Rotation2d.fromDegrees(55.0));
-
-    public static final Pose2d R_IL = new Pose2d(16.6, 1.20, Rotation2d.fromDegrees(-55.0+180)); // SourceLeft
-    public static final Pose2d R_IR = new Pose2d(16.45, 7.01, Rotation2d.fromDegrees(55.0+180));
-
+    public static final Pose2d B_IL = new Pose2d(0.84, 6.74, Rotation2d.fromDegrees(-55.0)); // SourceLeft
+    public static final Pose2d B_IR = new Pose2d(1.49, 0.6, Rotation2d.fromDegrees(55.0));
+    public static final Pose2d R_IL = new Pose2d(16.7, 1.25, Rotation2d.fromDegrees(127.18)); // SourceLeft
+    public static final Pose2d R_IR = new Pose2d(16.16, 7.33, Rotation2d.fromDegrees(55.0+180));
 
 
 
+    /*
+    R_IL: 16.7, 1.25, 127.18
+    R_IR: 16.16 7.33 55+180
+
+    
+    */
     
 
     
